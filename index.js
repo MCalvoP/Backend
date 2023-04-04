ab495f3c93f66b2ab34254ffc5df1f878016a2b1
Manuel <manuelcalvoparra1@gmail.com>
2023-04-04 00:52:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+class ProductManager {
+    constructor() {
+        this.products = []
+    }
+    addProduct(product){
+        if(this.products.find(producto => producto.code == product.code)){
+            return "Producto existente"
+        } else{
+            this.products.push(product)
+        }
+    }
+    getProducts() {
+        return this.products
+    }
+
+    getProductById(id){
+        const product = this.products.find(producto => producto.id == id)
+
+        if(product) {
+            return product
+        }
+        return "Not found"
+    }
+    
+}
+
+class Product {
+    constructor(title = "", description = "", price = 0, thumbnail = "", code = "", stock = 0) {
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+        this.id = Product.incrementID()
+    }
+    static incrementID() {
+        if(this.idIncrement) {
+            this.idIncrement++
+        } else{
+            this.idIncrement = 1
+        }
+        return this.idIncrement
+    }
+}
+
+const product1 = new Product("Arroz", "Arroz", 200, "", "A01", 10)
+const product2 = new Product("Fideos", "Fideos", 210, "", "F01", 15)
+const product3 = new Product("Pollo", "Pollo", 350, "", "P01", 8)
+const product4 = new Product("Carne", "Carne", 400, "", "C01", 6)
+const product5 = new Product()
+
+const productManager = new ProductManager()
+productManager.addProduct(product1)
+productManager.addProduct(product2)
+console.log(productManager.addProduct(product1))
+console.log(productManager.getProductById(2))
+console.log(productManager.getProductById(5))
+console.log(productManager.getProducts())
