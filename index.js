9feaba054824dd4fa042a2a38b4b184b7132f6c8
Manuel <manuelcalvoparra1@gmail.com>
2023-04-07 18:42:21 -0300

    Correcciones en las validaciones

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ class ProductManager {
         this.products = []
     }
     addProduct(product){
+        if (!product.title || product.title === ""){
+            return console.error("Faltan datos")
+        }
+        if (!product.description || product.description === ""){
+            return console.error("Faltan datos")
+        }
+        if (!product.price || product.price === ""){
+            return console.error("Faltan datos")
+        }
+        if (!product.code || product.code === ""){
+            return console.error("Faltan datos")
+        }
+        if (!product.stock || product.stock === ""){
+            return console.error("Faltan datos")
+        }
         if(this.products.find(producto => producto.code == product.code)){
             return "Producto existente"
         } else{
@@ -45,15 +60,15 @@ class Product {
 }
 
 const product1 = new Product("Arroz", "Arroz", 200, "", "A01", 10)
-const product2 = new Product("Fideos", "Fideos", 210, "", "F01", 15)
+const product2 = new Product("Fideos", "Fideos", 200, "", "F01", 15)
 const product3 = new Product("Pollo", "Pollo", 350, "", "P01", 8)
 const product4 = new Product("Carne", "Carne", 400, "", "C01", 6)
 const product5 = new Product()
 
 const productManager = new ProductManager()
 productManager.addProduct(product1)
-productManager.addProduct(product2)
+productManager.addProduct(product4)
 console.log(productManager.addProduct(product1))
-console.log(productManager.getProductById(2))
+console.log(productManager.getProductById(1))
 console.log(productManager.getProductById(5))
 console.log(productManager.getProducts())
